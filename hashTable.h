2959f379073ade2e6a06f95471d2ebe4f8fe1440
root <root@SOHANI-PC>
2013-09-27 11:20:59 +0530

    Join successfully tested

---
+++ hashTable.h
@@ -0,0 +1,65 @@
+/*
+ * hashTable.h
+ *
+ *  Created on: 10-Apr-2013
+ *      Author: spec
+ */
+
+#include "constants.h"
+#ifndef HASHTABLE_H_
+#define HASHTABLE_H_
+
+#ifdef VMALLOC
+#include "vmalloc.h"
+#endif
+
+typedef struct grpRecHdr {
+    float aggregate;
+} grpRecHdr;
+
+typedef struct hashEntry {
+    void *ptr;
+    UINT8 hash;
+} hashEntry;
+
+typedef struct pageHash {
+    BITMAP valid[ENTRIES_PER_PAGE / BITS_PER_BYTE];
+    struct pageHash* pNextPage;
+    hashEntry entry[ENTRIES_PER_PAGE];
+} pageHash;
+
+typedef struct bucket {
+    pageHash *firstPage;
+} sBucket;
+
+typedef struct hashTbl {
+    sBucket *pBucket;
+    pageHash *pFreeList;
+    int HTBucketCount;
+} hashTbl;
+
+typedef struct overflowPartition {
+    char *pFreeSpaceBeg;
+    char *pPartitionBeg;
+    int freeSpace;
+} sOverflowPartition;
+
+#ifdef VMALLOC
+int initHT(Vmalloc_t *PCMStructPtr, UINT32 HTBucketCount);
+#else
+int initHT(UINT32 HTBucketCount);
+#endif
+void insertHashEntry(void* tuple, char* attr, UINT32 attrSize);
+UINT8 searchHashEntry(char* attr, UINT32 attrSize, void** returnEntryPtr,
+        void **pLastPage,
+        void **pLastIndex);
+
+void printMemoryRec();
+void printPCMRec();
+void testWriteout();
+void calculateFinalTally();
+int initOverflowPartitions();
+void processNewTupleByHashing(char* pAttr, float aggColValue, int pass);
+void freeSpace();
+
+#endif /* HASHTABLE_H_ */
