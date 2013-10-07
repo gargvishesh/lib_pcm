666530e0b54f76c4bcc825acaec7d940d212d224
root <root@SOHANI-PC>
2013-10-07 14:59:19 +0530

    More or less working VMALLOC sorting. Only Count variable not working with vmalloc, hence left as it is

---
+++ sorting.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   sorting.h
+ * Author: root
+ *
+ * Created on 5 October, 2013, 3:58 PM
+ */
+
+#ifndef SORTING_H
+#define	SORTING_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+typedef int (*compareTuples)(const void * p1, const void * p2);
+
+#ifdef VMALLOC
+#include "vmalloc.h"
+int sortMultiPivotAndUndo(Vmalloc_t *PCMStructPtr, char* arrayToBeSorted, UINT32 elemCount, UINT32 elemSize, 
+        compareTuples compareFunc, char* outputBuffer);
+#else
+int sortMultiPivotAndUndo(char* arrayToBeSorted, UINT32 elemCount, UINT32 elemSize, 
+        compareTuples compareFunc, char* outputBuffer);
+#endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SORTING_H */
+
