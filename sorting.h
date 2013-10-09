9af88157b78e64127f42195deee761ec1c0f9c44
root <root@SOHANI-PC>
2013-10-09 21:43:41 +0530

    Simple Pos-only sorting with No Undo

--- sorting.h
+++ sorting.h
@@ -17,10 +17,10 @@ typedef int (*compareTuples)(const void * p1, const void * p2);
 #ifdef VMALLOC
 #include "vmalloc.h"
 int sortMultiPivotAndUndo(Vmalloc_t *PCMStructPtr, char* arrayToBeSorted, UINT32 elemCount, UINT32 elemSize, 
-        compareTuples compareFunc, char* outputBuffer);
+        compareTuples compareFunc, char* outputBuffer, UINT32 numPartitions, UINT32 maxThreshhold);
 #else
 int sortMultiPivotAndUndo(char* arrayToBeSorted, UINT32 elemCount, UINT32 elemSize, 
-        compareTuples compareFunc, char* outputBuffer);
+        compareTuples compareFunc, char* outputBuffer, UINT32 numPartitions, UINT32 maxThreshhold);
 #endif
 
 #ifdef	__cplusplus
